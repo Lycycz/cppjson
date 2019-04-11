9561f89b536ed37a9bd46f2dfe002fdd78eee02c
Lycycz <489271038@qq.com>
2019-04-11 12:56:55 +0800

    init

---
+++ build/CMakeFiles/CppJson.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppJson.dir/JSON.cpp.o"
+  "CMakeFiles/CppJson.dir/test.cpp.o"
+  "CppJson.pdb"
+  "CppJson"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppJson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
