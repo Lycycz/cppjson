17097064416c747bf1705543826e95ee7a597947
liyuchen <liyuchen@tf.com>
2019-12-27 17:33:26 +0800

    parser

--- include/JSON.hpp
+++ include/JSON.hpp
@@ -1,78 +1,118 @@
 #pragma once
 
-#include<iostream>
-#include<initializer_list>
-#include<map>
-#include<string>
-#include<vector>
+#include <ciso646>
+#include <initializer_list>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 
-class JSON
-{
+class JSON {
 public:
-    enum class value_type : uint8_t
-    {
-        array = 0,
-        object,
-        null,
-        string,
-        boolean,
-        number,
-        number_float
-    };
-    using object_t = std::map<std::string, JSON>;
-    using array_t = std::vector<JSON>;
-    using string_t = std::string;
-    using boolean_t = bool;
-    using number_t = int;
-    using number_float_t = double;
-    using list_init_t = std::initializer_list<JSON>;
-    union value
-    {
-        array_t *array;
-        object_t *object;
-        string_t *string;
-        boolean_t boolean;
-        number_t number;
-        number_float_t number_float;
-    };
+  enum class value_type : uint8_t {
+    array = 0,
+    object,
+    null,
+    string,
+    boolean,
+    number,
+    number_float
+  };
+  using object_t = std::map<std::string, JSON>;
+  using array_t = std::vector<JSON>;
+  using string_t = std::string;
+  using boolean_t = bool;
+  using number_t = int;
+  using number_float_t = double;
+  using list_init_t = std::initializer_list<JSON>;
+  union value {
+    array_t *array;
+    object_t *object;
+    string_t *string;
+    boolean_t boolean;
+    number_t number;
+    number_float_t number_float;
+  };
+
 public:
-    JSON() = default;
-    JSON(const std::string&) noexcept;
-    JSON(std::string &&) noexcept;
-    JSON(const char *) noexcept;
-    JSON(const bool) noexcept;
-    JSON(const int) noexcept;
-    JSON(const double) noexcept;
-    JSON(const array_t &) noexcept;
-    JSON(array_t &&) noexcept;
+  JSON() = default;
+  JSON(const std::string &) noexcept;
+  JSON(std::string &&) noexcept;
+  JSON(const char *) noexcept;
+  JSON(const bool) noexcept;
+  JSON(const int) noexcept;
+  JSON(const double) noexcept;
+  JSON(const array_t &) noexcept;
+  JSON(array_t &&) noexcept;
 
-    JSON& operator=(JSON) noexcept;
-    value_type type() const noexcept;
-    const std::string toString() const noexcept;
+  JSON &operator=(JSON) noexcept;
+  value_type type() const noexcept;
+  const std::string toString() const noexcept;
 
-    friend std::ostream& operator<<(std::ostream& o, const JSON& j)
-    {
-        o << j.toString();
-        return o;
-    }
-    /// write to stream
-    friend std::ostream& operator>>(const JSON& j, std::ostream& o)
-    {
-        o << j.toString();
-        return o;
-    }
+  friend std::ostream &operator<<(std::ostream &o, const JSON &j) {
+    o << j.toString();
+    return o;
+  }
+  /// write to stream
+  friend std::ostream &operator>>(const JSON &j, std::ostream &o) {
+    o << j.toString();
+    return o;
+  }
 
-    JSON& operator+=(const JSON&);
-    JSON& operator+=(const std::string&);
-    JSON& operator+=(const char*);
+  JSON &operator+=(const JSON &);
+  JSON &operator+=(const std::string &);
+  JSON &operator+=(const char *);
 
-    void push_back(const JSON&);
-    void push_back(JSON &&);
+  void push_back(const JSON &);
+  void push_back(JSON &&);
 
 private:
-    const std::string _typename() const noexcept;
+  const std::string _typename() const noexcept;
 
 private:
-    value_type _type = value_type::null;
-    value _value{};
+  value_type _type = value_type::null;
+  value _value{};
+
+  void *_payload;
+
+public:
+  class const_iterator;
+
+public:
+  class iterator {
+    friend class JSON::const_iterator;
+
+  public:
+    iterator();
+    iterator(JSON *);
+    iterator(const iterator &);
+    ~iterator();
+
+  private:
+    JSON *_object;
+    std::vector<JSON>::iterator *_vi;
+    std::map<std::string, JSON>::iterator *_oi;
+  };
+
+public:
+    iterator begin();
+    iterator end();
+    const_iterator begin() const;
+    const_iterator end() const;
+    const_iterator cbegin() const;
+    const_iterator cend() const;
+
+private:
+    class parser {
+    public:
+        parser(char*);
+        parser(std::string&);
+        parser(std::istream&);
+    private:
+        bool next();
+
+        char _current;
+        char *_buffer;
+        size_t _pos;
+    };
 };
--- src/JSON.cpp
+++ src/JSON.cpp
@@ -1,143 +1,157 @@
 #include "JSON.hpp"
 #include <iostream>
 
-JSON::JSON(const std::string& s) noexcept
-    : _type(value_type::string)
-{
-    _value.string = new string_t(s);
+JSON::JSON(const std::string &s) noexcept : _type(value_type::string) {
+  _value.string = new string_t(s);
 }
-JSON::JSON(std::string&& s) noexcept
-    : _type(value_type::string)
-{
-    _value.string = new string_t(std::move(s));
+JSON::JSON(std::string &&s) noexcept : _type(value_type::string) {
+  _value.string = new string_t(std::move(s));
 }
-JSON::JSON(const char* s) noexcept
-    : _type(value_type::string)
-{
-    _value.string = new string_t(s);
+JSON::JSON(const char *s) noexcept : _type(value_type::string) {
+  _value.string = new string_t(s);
 }
-JSON::JSON(const bool b) noexcept
-    :_type(value_type::boolean)
-{
-    _value.boolean = b;
+JSON::JSON(const bool b) noexcept : _type(value_type::boolean) {
+  _value.boolean = b;
 }
-JSON::JSON(const int i) noexcept
-    : _type(value_type::number)
-{
-    _value.number = i;
+JSON::JSON(const int i) noexcept : _type(value_type::number) {
+  _value.number = i;
 }
-JSON::JSON(const double f) noexcept
-    :_type(value_type::number_float)
-{
-    _value.number_float = f;
+JSON::JSON(const double f) noexcept : _type(value_type::number_float) {
+  _value.number_float = f;
 }
-JSON::JSON(const array_t& a) noexcept
-    : _type(value_type::array)
-{
-    _value.array = new array_t(a);
+JSON::JSON(const array_t &a) noexcept : _type(value_type::array) {
+  _value.array = new array_t(a);
 }
-JSON::JSON(array_t&& a ) noexcept
-    :_type(value_type::array)
-{
-    _value.array = new array_t(std::move(a));
+JSON::JSON(array_t &&a) noexcept : _type(value_type::array) {
+  _value.array = new array_t(std::move(a));
 }
-JSON::value_type JSON::type() const noexcept
-{
-    return _type;
+JSON::value_type JSON::type() const noexcept { return _type; }
+const std::string JSON::toString() const noexcept {
+  switch (_type) {
+  case value_type::null: {
+    return "null";
+  }
+  case value_type::string: {
+    return "\"" + *_value.string + "\"";
+  }
+  case value_type::number: {
+    return std::to_string(_value.number);
+  }
+  case value_type::number_float: {
+    return std::to_string(_value.number_float);
+  }
+  case value_type::boolean: {
+    return _value.boolean ? "true" : "false";
+  }
+  }
 }
-const std::string JSON::toString() const noexcept
-{
-    switch (_type)
-    {
-        case value_type::null:
-        {
-            return "null";
-        }
-        case value_type::string:
-        {
-            return "\"" + *_value.string + "\"";
-        }
-        case value_type::number:
-        {
-            return std::to_string(_value.number);
-        }
-        case value_type::number_float:
-        {
-            return std::to_string(_value.number_float);
-        }
-        case value_type::boolean:
-        {
-            return _value.boolean?"true":"false";
-        }
-    }
-}
-JSON& JSON::operator=(JSON o) noexcept
-{
-    std::swap(_type,o._type);
-    std::swap(_value,o._value);
-    return *this;
+JSON &JSON::operator=(JSON o) noexcept {
+  std::swap(_type, o._type);
+  std::swap(_value, o._value);
+  return *this;
 }
 
-JSON& JSON::operator+=(const JSON& o)
-{
-    push_back(o);
-    return *this;
+JSON &JSON::operator+=(const JSON &o) {
+  push_back(o);
+  return *this;
 }
-JSON& JSON::operator+=(const std::string& s)
-{
-    push_back(JSON(s));
-    return *this;
+JSON &JSON::operator+=(const std::string &s) {
+  push_back(JSON(s));
+  return *this;
 }
-JSON& JSON::operator+=(const char*s)
-{
-    push_back(JSON(s));
-    return *this;
+JSON &JSON::operator+=(const char *s) {
+  push_back(JSON(s));
+  return *this;
 }
-void JSON::push_back(const JSON& o)
-{
-    if(not(_type == value_type::array or _type == value_type::null))
-    {
-        throw std::runtime_error("cannot add element to" + _typename());
-    }
+void JSON::push_back(const JSON &o) {
+  if (not(_type == value_type::array or _type == value_type::null)) {
+    throw std::runtime_error("cannot add element to" + _typename());
+  }
 
-    if (_type == value_type::null)
-    {
-        _type = value_type::array;
-        _value.array = new array_t;
-    }
+  if (_type == value_type::null) {
+    _type = value_type::array;
+    _value.array = new array_t;
+  }
 
-    _value.array->push_back(o);
+  _value.array->push_back(o);
 }
-void JSON::push_back(JSON&& o)
-{
-    if(not(_type == value_type::array or _type == value_type::null))
-    {
-        throw std::runtime_error("cannot add element to" + _typename());
-    }
+void JSON::push_back(JSON &&o) {
+  if (not(_type == value_type::array or _type == value_type::null)) {
+    throw std::runtime_error("cannot add element to" + _typename());
+  }
 
-    if (_type == value_type::null)
-    {
-        _type = value_type::array;
-        _value.array = new array_t;
-    }
+  if (_type == value_type::null) {
+    _type = value_type::array;
+    _value.array = new array_t;
+  }
 
-    _value.array->emplace_back(std::move(o));
+  _value.array->emplace_back(std::move(o));
 }
-const std::string JSON::_typename() const noexcept
-{
-    switch (_type)
-    {
-    case value_type::array:
-        return "array";
-    case value_type::object:
-        return "object";
-    case value_type::null:
-        return "null";
-    case value_type::boolean:
-        return "boolean";
-    case value_type::number:
-        return "number";
-    case value_type::number_float:
-        return "number";
-    }   
+const std::string JSON::_typename() const noexcept {
+  switch (_type) {
+  case value_type::array:
+    return "array";
+  case value_type::object:
+    return "object";
+  case value_type::null:
+    return "null";
+  case value_type::string:
+    return "string";
+  case value_type::boolean:
+    return "boolean";
+  case value_type::number:
+    return "number";
+  case value_type::number_float:
+    return "number";
+  }
+}
+
+JSON::iterator::iterator() : _object(nullptr), _vi(nullptr), _oi(nullptr) {}
+
+JSON::iterator::iterator(JSON *j) : _object(j), _vi(nullptr), _oi(nullptr) {
+  switch (_object->_type) {
+  case (value_type::array): {
+    _vi = new std::vector<JSON>::iterator(
+        static_cast<std::vector<JSON> *>(_object->_payload)->begin());
+  }
+  case (value_type::object): {
+    _oi = new std::map<std::string, JSON>::iterator(
+        static_cast<std::map<std::string, JSON> *>(_object->_payload)->begin());
+  }
+  default:
+    break;
+  }
+}
+
+JSON::iterator::iterator(const iterator &o)
+    : _object(o._object), _vi(nullptr), _oi(nullptr) {
+  switch (_object->_type) {
+  case (value_type::array): {
+    _vi = new std::vector<JSON>::iterator(
+        static_cast<std::vector<JSON> *>(_object->_payload)->begin());
+  }
+  case (value_type::object): {
+    _oi = new std::map<std::string, JSON>::iterator(
+        static_cast<std::map<std::string, JSON> *>(_object->_payload)->begin());
+  }
+  default:
+    break;
+  }
+}
+
+JSON::iterator::~iterator() {
+  delete _oi;
+  delete _vi;
+}
+
+JSON::iterator JSON::begin() {
+    return JSON::iterator(this);
+}
+
+JSON::iterator JSON::end() {
+    return JSON::iterator();
+}
+
+JSON::parser::parser(char* s) {
+    
 }
